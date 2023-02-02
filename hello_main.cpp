061765b91e000f7435bd75fb26da643148d8e0f7
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-02-01 19:48:49 -0800

    hello_main assignment completed

---
+++ hello_main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello my name is Carlos. " << endl;
+    cout << "This is my last semester at Mission College" << endl;
+    cout << "I begin my first semester as a computer science student at Cal State East Bay in the fall." << endl;
+    cout << "My hoobies include spending time with my family, learning about computer science concepts, and watching NBA basketball." << endl;
+    cout << "I am excited to learn more about C++ and also about OOP. I've been looking forward to this class!" << endl;
+    return 0;
+}
