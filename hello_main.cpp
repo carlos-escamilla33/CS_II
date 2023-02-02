6d2995f0e4d0f4b84c5ee3cb9389f690b3e59a8e
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-02-01 19:51:58 -0800

    added output comment to hello_main

--- hello_main.cpp
+++ hello_main.cpp
@@ -10,3 +10,12 @@ int main()
     cout << "I am excited to learn more about C++ and also about OOP. I've been looking forward to this class!" << endl;
     return 0;
 }
+
+
+/*
+Hello my name is Carlos. 
+This is my last semester at Mission College
+I begin my first semester as a computer science student at Cal State East Bay in the fall.
+My hoobies include spending time with my family, learning about computer science concepts, and watching NBA basketball.
+I am excited to learn more about C++ and also about OOP. I've been looking forward to this class!
+*/
