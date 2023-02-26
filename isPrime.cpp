ae7ff96fe3532842e4db97bdbc938dae987fd032
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-02-25 23:29:55 -0800

    finishing up isPrime hw

--- isPrime.cpp
+++ isPrime.cpp
@@ -1,28 +1,49 @@
 #include <iostream>
 #include <cctype>
 #include <vector>
+#include <string>
 using namespace std;
 
-bool isPrime(int n) {
-    int divBy = 1;
-    // if (n)
-    return 0;
+bool isPrime(int n) { // possible refactor of this function
+    bool isPrimeNum;
+    int size = n;
+    int primesCount = 0;
+
+    for (int i = 1; i <= size; i++) {
+        if (n % i == 0) {
+            primesCount+=1;
+        }
+    }
+    if (primesCount != 2) {
+        isPrimeNum = false;
+    }
+
+    return isPrimeNum;
 }
 
 
 int main() {
     vector<int> primes_list;
     int userNum;
-    unsigned int i;
+    string userResponse;
 
     while (true) {
-        cout << "Enter a positive number: " << endl;
+        cout << "Enter a positive number: ";
         cin >> userNum;
-
-        if (isPrime(userNum)) {
-            primes_list.at(i) = userNum;
+        if (isPrime(userNum)) { // check variable data type
+            primes_list.push_back(userNum);
+        }
+        cout << "Do you want to enter a new number? ";
+        cin >> userResponse;
+        if (userResponse == "no") { // refactor to be more efficient and clear
+            break;
         }
     }
+
+    for (int i = 0; i < primes_list.size(); i++) {
+        cout << primes_list[i] << " " << endl;
+    }
+
     return 0;
 }
 
