9310f30ebc2b7749cc248715da8ee1aa2720319a
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-02-26 21:15:26 -0800

    finished isPrime

--- isPrime.cpp
+++ isPrime.cpp
@@ -1,49 +1,43 @@
 #include <iostream>
 #include <cctype>
 #include <vector>
-#include <string>
+#include <cmath>
 using namespace std;
 
-bool isPrime(int n) { // possible refactor of this function
-    bool isPrimeNum;
-    int size = n;
-    int primesCount = 0;
-
-    for (int i = 1; i <= size; i++) {
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    int maxSize = sqrt(n);
+    for (int i = 2; i <= maxSize; i++) {
         if (n % i == 0) {
-            primesCount+=1;
+            return false;
         }
     }
-    if (primesCount != 2) {
-        isPrimeNum = false;
-    }
-
-    return isPrimeNum;
+    return true;
 }
-
 
 int main() {
     vector<int> primes_list;
-    int userNum;
-    string userResponse;
-
     while (true) {
+        int userNum;
+        char userResponse;
         cout << "Enter a positive number: ";
         cin >> userNum;
-        if (isPrime(userNum)) { // check variable data type
-            primes_list.push_back(userNum);
+        for (int i = 1; i <= userNum; i++) {
+            bool result = isPrime(i);
+            if (result && !(count(primes_list.begin(), primes_list.end(), i))) {
+                primes_list.push_back(i);
+            }
         }
-        cout << "Do you want to enter a new number? ";
+        cout << "Do you want to enter a new number, type Y or N: ";
         cin >> userResponse;
-        if (userResponse == "no") { // refactor to be more efficient and clear
+        if (toupper(userResponse) == 'N') {
             break;
         }
     }
-
     for (int i = 0; i < primes_list.size(); i++) {
         cout << primes_list[i] << " " << endl;
     }
-
     return 0;
 }
-
