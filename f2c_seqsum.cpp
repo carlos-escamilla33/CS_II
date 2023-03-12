ad40f09482da342934f2a1f98c9f346b34a93fb5
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-03-11 22:47:38 -0800

    finished f2c hw

--- f2c_seqsum.cpp
+++ f2c_seqsum.cpp
@@ -1,14 +1,52 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int f2c(int fahrenheit);
+double f2c(double fahrenheit);
+void seqsum(int n);
 
 int main() {
-    
+    double userTemp;
+    double tempInCelsius;
+    const int maxnum = 1000;
+    int n;
+    int i = 0;
+    int runningSum;
+
+    cout << "Enter temp in fahrenheit: ";
+    cin >> userTemp;
+    tempInCelsius = f2c(userTemp);
+
+    cout << setprecision(4) << endl;
+    cout << userTemp << " degrees fahrenheit = " << tempInCelsius << " degrees celsius" << endl << endl;
+
+    cout << "Enter number greater than 0: ";
+    cin >> n;
+
+    cout << n << ":  ";
+
+    while (n > 0 && n <=1000) {
+        if (n < 1 || n > maxnum) {
+            break;
+        }
+        cout << i + 1;
+        if (n != 1) {
+            cout << " + ";
+        }
+        runningSum+=n;
+        i++;
+        n--;
+    }
+    cout << " = " << runningSum;
+
     return 0;
 }
 
-int f2c(int fahrenheit) {
-    int celcius = (fahrenheit - 32) * (5/9);
+double f2c(double fahrenheit) {
+    double celcius = (fahrenheit - 32) * 0.556;
     return celcius;
+}
+
+void seqsum(int n) {
+
 }
