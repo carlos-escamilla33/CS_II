51417c062d8786a9ea3ef7ff2c5339cbbf62007f
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-03-31 21:19:36 -0700

    phone assignment created

---
+++ phone/phone.cpp
@@ -0,0 +1,5 @@
+#include "phone.hpp"
+
+bool isValid(string &line) {
+    
+}
---
+++ phone/phone.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cstdbool> // for true or false
+#include <cctype> //https://www.cplusplus.com/reference/cctype/
+using namespace std;
+
+bool isValid(string &line);
