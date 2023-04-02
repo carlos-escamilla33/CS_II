cceede1670c6e81a76f00003fd3a744866aa5a22
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-04-01 21:37:03 -0700

    initialized all functions for phone.hpp

--- phone/phone.hpp
+++ phone/phone.hpp
@@ -8,3 +8,9 @@ using namespace std;
 
 bool isValid(string &line);
 char phone_digit(char ch);
+void toNumeric(string &line);
+void puncts_to_spaces(string &line);
+void removeSpaces2(string &line);
+void remove_spaces(string &line);
+void formatNumber2(string &line);
+void format_number(string &line);
