b7a7e535890103b601c8dfd17887de7bd7189c29
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-04-30 00:18:54 -0700

    set Car operator and constructor/deconstructor public functions

--- car/car.h
+++ car/car.h
@@ -4,6 +4,13 @@ using namespace std;
 
 class Car {
     public:
+        Car(); // default constructor (1)
+        ~Car(); // default deconstructor (2)
+        Car(const Car& c); // assignment operator (3)
+        Car& operator=(const Car& c);
+        Car(string& modelName, int modelYear);
+
+        // Set functions; modifiers; read/write
 
     private:
         static int count;
