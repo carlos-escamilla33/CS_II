c9d28fe0e10b1ac2de56a2681f791d752bb504e5
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-05-07 23:13:58 -0700

    creating practice .cpp, .h, main.cpp files

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,27 +1,2 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
-int main() {
-    int numValues;
-    unsigned int i;
-    vector<double> valsList;
-
-    cout << "Enter vector size: ";
-    cin >> numValues;
-
-    valsList.resize(numValues);
-
-    cout << "Enter values for vector 0 and 1 index: ";
-    cin >> valsList.at(0);
-    cin >> valsList.at(1);
-
-    for (i = 1; i < valsList.size() - 1; i++) {
-        double currSum = valsList.at(i - 1) + valsList.at(i);
-        valsList.at(i + 1) = currSum;
-        cout << currSum << " " << i;
-        cout << endl;
-    }
-
-    return 0;
-}
